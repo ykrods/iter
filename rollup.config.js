3ae25fe6d9b04fc87d1223f0ee332a8987ba7514
ykrods <890082+ykrods@users.noreply.github.com>
2021-10-22 16:55:36 +0900

    Initial svelte project

---
+++ rollup.config.js
@@ -0,0 +1,62 @@
+import svelte from 'rollup-plugin-svelte';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import css from 'rollup-plugin-css-only';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default [{
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    name: 'app', // export window.app
+    format: 'esm',
+    dir: 'public/dist',
+    chunkFileNames: '[name].js'
+  },
+  external: [
+  ],
+  plugins: [
+    svelte({
+      extensions: ['.svelte', '.svg'],
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production,
+      },
+    }),
+
+    // we'll extract any component CSS out into
+    // a separate file - better for performance
+    css({ output: 'bundle.css' }),
+
+    // to import packages in node_modules
+    nodeResolve({
+      browser: true,
+      dedupe: ['svelte']
+    }),
+
+    !production && serve(),
+    production && terser(),
+  ],
+  watch: {
+    include: 'src/**',
+    chokidar: false,
+    clearScreen: false,
+  },
+}];
+
+function serve() {
+  let started = false;
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true;
+
+        require("child_process").spawn('npm', ['run', 'start'], {
+          stdio: ['ignore', 'inherit', 'inherit'],
+          shell: true
+        });
+      }
+    }
+  };
+}
