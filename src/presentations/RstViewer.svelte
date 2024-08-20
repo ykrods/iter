3d3b4ae83ae1bc2a452bffafc209c8c0d77610c1
ykrods <890082+ykrods@users.noreply.github.com>
2024-08-20 19:00:27 +0900

    Enable link in rst document

--- src/presentations/RstViewer.svelte
+++ src/presentations/RstViewer.svelte
@@ -3,6 +3,8 @@
   import "$src/styles/docutils-0.20.1/math.css";
   import "$src/styles/pygments-2.16.1/default.css";
 
+  import { push } from "svelte-spa-history-router";
+
   import { asyncWorkerClient } from "$src/lib/asyncWorkerClient";
 
   let { rst } : { rst: string } = $props();
@@ -18,8 +20,30 @@
   async function loadHTML(rst) {
     html = await client.rst2html(rst);
   }
+
+  function captureClick(event) {
+    if (event.target.tagName.toLowerCase() !== 'a') {
+      return;
+    }
+    // Ignore external link
+    if (event.target.hostname !== window.location.hostname) {
+      event.preventDefault();
+      window.open(event.target.href, "_blank", "noopener,noreferrer");
+      return;
+    }
+    // Ignore fragment jump
+    if (event.target.pathname === window.location.pathname &&
+        event.target.hash !== window.location.hash) {
+      return;
+    }
+    if (event.target.pathname) {
+      event.preventDefault();
+      push(event.target.pathname);
+    }
+  }
 </script>
-<div>
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<div role="none" onclick={captureClick}>
   {#if html != ""}
     {@html html}
   {/if}
