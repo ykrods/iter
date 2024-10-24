381c646b828b6da21bd7b79dbeeb530793236923
ykrods <890082+ykrods@users.noreply.github.com>
2024-10-24 23:16:00 +0900

    Add shoelace components

---
+++ src/ui/shoelace/SLIcon.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import SlIcon from "@shoelace-style/shoelace/dist/components/icon/icon";
+
+  let props: {
+    name: SlIcon["name"]
+    label?: SlIcon["label"]
+  } = $props();
+</script>
+<sl-icon {...props}></sl-icon>
