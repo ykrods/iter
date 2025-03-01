3264fce273e9b1866d612eef931496e5cd533b13
ykrods <890082+ykrods@users.noreply.github.com>
2025-03-01 17:38:04 +0900

    Rebuild

--- src/pages/NotFound.svelte
+++
@@ -1,6 +0,0 @@
-<svelte:head>
-  <title>not found | iter</title>
-</svelte:head>
-<main id="NotFound">
-  NotFound
-</main>
--- src/ui/buttons/MenuButton.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { Button, Icon } from 'svelte-mui';
-
-  import MenuIcon from '../../icons/menu.svg';
-</script>
-
-<Button on:click icon color='#00796b' unelevated dense>
-  <Icon><svelte:component this={MenuIcon}/></Icon>
-</Button>
