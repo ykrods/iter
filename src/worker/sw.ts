ed3b9ccff333aad6fdbbb557c3087e8e26d363a8
ykrods <890082+ykrods@users.noreply.github.com>
2025-04-29 21:08:14 +0900

    setup pyodide

--- src/worker/sw.ts
+++ src/worker/sw.ts
@@ -1,11 +1,54 @@
-self.addEventListener('install', (event) => {
+import type {
+  PyodideInterface,
+  loadPyodide as loadPyodide_orig,
+} from "pyodide";
+
+declare global {
+  var loadPyodide: typeof loadPyodide_orig
+}
+
+importScripts("/_/static/pyodide/pyodide.js");
+
+let pyodide: PyodideInterface;
+
+
+let pyodideReadyPromise = (async () => {
+  pyodide = await loadPyodide();
+
+  // TODO: serve wheel files
+  await pyodide.loadPackage([
+    "https://cdn.jsdelivr.net/pyodide/v0.27.3/full/docutils-0.21.1-py3-none-any.whl",
+    "https://cdn.jsdelivr.net/pyodide/v0.27.3/full/pygments-2.17.2-py3-none-any.whl"
+  ]);
+
+  const oneShotRun = (code: string) => {
+    const scope = pyodide.globals.get("dict")();
+    const result = pyodide.runPython(code, { globals: scope });
+    scope.destroy();
+    return result;
+  };
+
+  // oneShotRun(display_versions);
+  // save as pygments-default.css
+  // console.log(oneShotRun(gen_pygments_style));
+
+  // pyodide.runPython(rst2html_py);
+})();
+
+self.addEventListener('install', async (event: ExtendableEvent) => {
   console.log("install");
+
+  await pyodideReadyPromise;
+  pyodide.runPython("print(1 + 1)");
+
+  // TODO: Create cache
+
   // activate installed worker without
   // waiting for the open page to be closed
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', (event: ExtendableEvent) => {
   console.log("activate");
   // activate newest worker without
   // waiting for reloading when registered
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import spawn from "./vite-plugin-spawn"
+import copy from "rollup-plugin-simple-copy/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
     spawn("npm", ["run", "build:sw", "--", "--watch"]),
+    copy({
+      extMap: {
+        ".svg": "image/svg+xml",
+        ".html": "text/html", // for pyodide console.html
+        ".js": "text/javascript",
+        ".mjs": "text/javascript",
+        ".json": "application/json",
+        ".zip": "application/zip",
+        ".wasm": "application/wasm",
+      },
+      targets: [
+        {
+          src: "node_modules/pyodide",
+          dest: "_/static/pyodide",
+        },
+      ]
+    })
   ],
 })
